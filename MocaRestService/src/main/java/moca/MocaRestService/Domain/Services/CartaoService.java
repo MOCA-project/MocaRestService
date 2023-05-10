fc7be040be614a2dc0b7c34f57c96012dde0ca75
Alexandre Costa <alexandre.rodrigues@genial.com.vc>
2023-05-10 20:19:07 -0300

    bugfix - parallel stream return only after all operation is completed

--- MocaRestService/src/main/java/moca/MocaRestService/Domain/Services/CartaoService.java
+++ MocaRestService/src/main/java/moca/MocaRestService/Domain/Services/CartaoService.java
@@ -12,6 +12,10 @@ import moca.MocaRestService.Domain.Models.Responses.CartoesHomeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 @Service
 public class CartaoService {
 
@@ -36,27 +40,36 @@ public class CartaoService {
 
         var response = new CartoesHomeResponse();
         var cartoes = repository.findByIdCliente(idCliente);
-        long startTime = System.currentTimeMillis();
-        cartoes.parallelStream().forEach(cartao -> {
-            var gasto = despesasRepository.getGastosCartoesTotal(idCliente, cartao.getIdCartao());
-            response.add(new CartoesHomeCartao(
-                    cartao.getLimite(),
-                    gasto,
-                    getPorcentagem(cartao.getLimite(), gasto),
-                    cartao.getVencimento(),
-                    cartao.getIdCorCartao(),
-                    cartao.getBandeira(),
-                    cartao.getApelido(),
-                    cartao.getIdCartao()
-            ));
-        });
-        long endTime = System.currentTimeMillis();
-        System.out.println("Tempo de execução do método get(): " + (endTime - startTime) + " ms");
+
+        List<CompletableFuture<CartoesHomeCartao>> futures = cartoes.parallelStream()
+                .map(cartao -> CompletableFuture.supplyAsync(() -> {
+                    var gasto = despesasRepository.getGastosCartoesTotal(idCliente, cartao.getIdCartao());
+                    return new CartoesHomeCartao(
+                            cartao.getLimite(),
+                            gasto,
+                            getPorcentagem(cartao.getLimite(), gasto),
+                            cartao.getVencimento(),
+                            cartao.getIdCorCartao(),
+                            cartao.getBandeira(),
+                            cartao.getApelido(),
+                            cartao.getIdCartao()
+                    );
+                }))
+                .collect(Collectors.toList());
+
+        List<CartoesHomeCartao> cartoesHome = futures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
         return response;
     }
 
     private static double getPorcentagem(double total, double utilizado){
         double porcentagem =  (utilizado * 100) / total;
         return Math.floor(porcentagem * 100) / 100;
+    }
+
+    public void remove(long idCartao) {
+         repository.deleteById(idCartao);
     }
 }
